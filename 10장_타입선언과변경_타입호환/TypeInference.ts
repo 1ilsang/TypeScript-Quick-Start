970f1388f7f12004356ae80a9472d98fbe21b2ab
LEE SANG-CHUL <1ilsang@naver.com>
2018-05-08 20:30:08 +0900

    type inference

---
+++ 10장_타입선언과변경_타입호환/TypeInference.ts
@@ -0,0 +1,7 @@
+let values = [0, "c", null];
+console.log(typeof values);
+console.log(typeof values[0]);
+console.log(typeof values[1]);
+console.log(typeof values[2]);
+values[0] = values[1]
+console.log(typeof values[0]);
